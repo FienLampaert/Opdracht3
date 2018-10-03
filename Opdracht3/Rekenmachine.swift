dc068771a8fc48aba685e16e290cb34d1e5dc90a
student <student@vh-pjrh092016.local>
2018-10-03 10:39:21 +0200

    Initial Commit

---
+++ Opdracht3/Rekenmachine.swift
@@ -0,0 +1,45 @@
+//
+//  Rekenmachine.swift
+//  Opdracht3
+//
+//  Created by student on 3/10/18.
+//  Copyright © 2018 nielsbrackeva. All rights reserved.
+//
+
+import Foundation
+
+
+class Rekenmachine{
+    var getallen: [Double]
+    
+    init(){
+        getallen = [];
+    }
+    
+     func clearStack(){
+        getallen.removeAll();
+     }
+     
+     func addOperand(Operand operand: Double){
+        getallen.append(operand);
+     }
+     
+     func performOperation(Operator operation: String) -> (result: Double?, feedback: String){
+     
+        let o = operation;
+        var result = 0.0;
+        
+        if(getallen.count >= 2){
+            let g1 = getallen[0];
+            let g2 = getallen[1];
+            
+            result = g1 + g2;
+        }
+        
+        return result
+        
+     }
+    
+}
+
+
---
+++ Opdracht3/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Opdracht3
+//
+//  Created by student on 27/09/18.
+//  Copyright © 2018 nielsbrackeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtStack: UITextView!
+    @IBOutlet weak var lblFeedback: UILabel!
+    @IBAction func bntCijfer(_ sender: Any) {
+    }
+    
+    @IBAction func btnEnter(_ sender: Any) {
+    }
+    
+    @IBAction func btnOperation(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+}
+
